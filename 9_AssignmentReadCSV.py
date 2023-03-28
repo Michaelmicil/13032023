7243f9627a69a39fdea3a777233c75400efae4ec
Michael <Michael77wong@gmail.com>
2023-03-28 18:15:51 +0700

    9_AssignmentReadCSV

--- 9_AssignmentReadCSV.py
+++ 9_AssignmentReadCSV.py
@@ -1,17 +1,13 @@
-#
-# Template for Python program
-#
 import pandas as pd
 
 # 1.Input
-raw_data = pd.read_csv("AssignmentMenu.csv")
+raw_data = pd.read_csv("AssignmentMenu1.csv")
 print(raw_data)
-print(raw_data.info())
 
 # 2. Process
+sorted_data = raw_data.sort_values("Price",ascending=False)
 total = raw_data["Price"].sum()
 
-
-
 # 3. Output
+print(f'Sorted:{sorted_data}')
 print(f"Total:{total}")
