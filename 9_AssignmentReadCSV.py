a2bd345d1e37daf7addec91fe202409639d2ebd7
Michael <Michael77wong@gmail.com>
2023-03-28 22:31:06 +0700

    AssignmentReadCSV

--- 9_AssignmentReadCSV.py
+++ 9_AssignmentReadCSV.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # 1.Input
-raw_data = pd.read_csv("AssignmentMenu1.csv")
+raw_data = pd.read_csv("AssignmentReadCSV.csv")
 print(raw_data)
 
 # 2. Process
-sorted_data = raw_data.sort_values("Menu",ascending=True)
-total = len(raw_data)
+Numberofitems = len(raw_data)
+Sorted_data = raw_data.sort_values ("Menu",ascending=True)
 
 # 3. Output
-print(f'Sorted:{sorted_data}')
-print(f"Total:{total}")
+print(f"Count:{Numberofitems}")
+print(f'{Sorted_data}')
