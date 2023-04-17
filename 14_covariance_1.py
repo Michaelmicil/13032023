a6140425f7ad10e9d07e405759f7b174dddb852e
Michael <Michael77wong@gmail.com>
2023-04-17 21:22:29 +0700

    Covariance Assugnment

--- 14_covariance_1.py
+++ 14_covariance_1.py
@@ -4,13 +4,13 @@
 import statistics, math
 
 def my_covariance(input_x, input_y):
-    print(f'Input x: {input_x}, Input y : {input_y}')
-    length_x = len(input_x)
-    length_y = len(input_y)
-    cov = 0 
-    mean_x = sum(input_x)/float(len(input_x))
-    mean_y = sum(input_y)/float(len(input_y))
-    print(f'Mean x : {mean_x}, Mean  y : {mean_y} ')
+    print(f'Input x:{input_x}, Input y: {input_y}')
+    length_x= len(input_x)
+    length_y= len(input_y)
+    cov=0
+    mean_x = sum(input_x)/ float(length_x)
+    mean_y = sum(input_y)/ float(length_y)
+    print(f'Mean x: {mean_x}, Mean y: {mean_y} ')
     cov = sum([(input_x[i] - mean_x) * (input_y[i] - mean_y) for i in range(length_x)]) / length_x
     output = math.sqrt(cov)
     return cov
