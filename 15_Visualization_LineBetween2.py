88fd3ac91b32bcb68b96431aa8a2cd9f48ca9949
Michael <Michael77wong@gmail.com>
2023-04-24 23:06:13 +0700

    Matplotlib 240423

---
+++ 15_Visualization_LineBetween2.py
@@ -0,0 +1,18 @@
+#
+# Template for python program
+# Name:
+#
+
+import matplotlib.pyplot as plt
+import numpy as np 
+
+# 1. Input
+xpoints= np.array([0,6])
+ypoint= np.array([0,250])
+
+# 2. Process
+
+# 3. Output
+plt.title("Line Between Two Point")
+plt.plot(xpoints,ypoint)
+plt.show()
